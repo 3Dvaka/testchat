57e2d3d624e51d454263b164a5c702c909385c26
3Dvaka <108063763+3Dvaka@users.noreply.github.com>
2025-02-02 21:06:07 +0200

    Update signin.jsx

--- das/PortfolioChat_FE/chat-frontend/src/components/ui/sign/signIn/signin.jsx
+++ das/PortfolioChat_FE/chat-frontend/src/components/ui/sign/signIn/signin.jsx
@@ -18,7 +18,7 @@ export default function SignIn({ switchToSignUp }) {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch("http://localhost:5000/api/auth/login", {
+            const response = await fetch("https://testchatback-production.up.railway.app/api/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(credentials),
